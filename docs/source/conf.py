55930630694de3cd62cbd72954c01f4616663f40
kewinbrand <33696153+kewinbrand@users.noreply.github.com>
2023-07-31 11:57:38 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Viasoft Core'
+copyright = '2023, Viasoft Korp'
+author = 'Viasoft Korp'
 
 release = '0.1'
 version = '0.1.0'
